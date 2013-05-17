72670aeec0f900e05898468d51b6d1004cf5f4b7
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-05-17 12:33:33 +0900

    Marged lib/content_unit for pagination

--- lib/paginate.rb
+++ lib/paginate.rb
@@ -1,6 +1,6 @@
 module Paginate
-  require File.expand_path(File.dirname(__FILE__) + '/contents_unit.rb')
-  include PaginateCondition 
+  #require File.expand_path(File.dirname(__FILE__) + '/contents_unit.rb')
+  #include PaginateCondition 
   FIRST_CONTENT = 1
 
   def content_paginate(page_number)
@@ -30,5 +30,26 @@ module Paginate
       objs << obj.set_params({:id => row["id"], :title => row["title"], :body => row["body"], :created_at => row["created_at"], :updated_at => row["updated_at"]})
     end
     objs
+  end
+
+  #####
+  #
+  # method for settings
+  #
+  ####
+
+
+  def contents_unit=(limit)
+    @contents_unit = limit.to_i
+  end
+  def contents_unit
+    @contents_unit
+  end
+
+  def sort_colomn=(limit)
+    @sort_colomn = limit.to_s
+  end
+  def sort_colomn
+    @sort_colomn
   end
 end
