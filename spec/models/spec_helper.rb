aead91beff9322f308e78a0008e8cf9c152a0094
Kotaro Kitamura <kotaro.kitamura@gaiax.com>
2013-06-11 12:16:13 +0900

    Get id from fixture data

--- spec/models/spec_helper.rb
+++ spec/models/spec_helper.rb
@@ -26,22 +26,20 @@ module SettingDb
     @contents_data = []
     if content == :blog
       fixture_contents.each_with_index do |fixture,num|
-        fixture_data << [fixture_contents["#{content}#{num+1}"]["title"],fixture_contents["#{content}#{num+1}"]["body"]]
+        fixture_data << [fixture_contents["#{content}#{num+1}"]["id"],fixture_contents["#{content}#{num+1}"]["title"],fixture_contents["#{content}#{num+1}"]["body"]]
       end
-      blog_id = 1
-      fixture_data.each do |title,body|
-        @client.query("INSERT INTO #{content}s (id,title,body,created_at,updated_at) VALUES ('#{blog_id}','#{title}','#{body}','#{Time.now}','#{Time.now}')")
+      fixture_data.each do |id,title,body|
+        @client.query("INSERT INTO #{content}s (id,title,body,created_at,updated_at) VALUES ('#{id}','#{title}','#{body}','#{Time.now}','#{Time.now}')")
         @contents_data << {:title => title, :body => body}
-        blog_id  += 1 
       end
     else
       @blog = Blog.new
       @blog.id = 1
       fixture_contents.each_with_index do |fixture,num|
-        fixture_data << [fixture_contents["#{content}#{num+1}"]["body"],fixture_contents["#{content}#{num+1}"]["blog_id"]]
+        fixture_data << [fixture_contents["#{content}#{num+1}"]["id"],fixture_contents["#{content}#{num+1}"]["body"],fixture_contents["#{content}#{num+1}"]["blog_id"]]
       end
-      fixture_data.each do |body,blog_id|
-        @client.query("INSERT INTO #{content}s(body,created_at,blog_id) VALUES ('#{body}','#{Time.now}','#{blog_id}')")
+      fixture_data.each do |id,body,blog_id|
+        @client.query("INSERT INTO #{content}s (id,body,created_at,blog_id) VALUES ('#{id}','#{body}','#{Time.now}','#{blog_id}')")
         @contents_data << ({:body => body, :blog_id => blog_id})
       end
     end
